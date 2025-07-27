335e19df8782aa1a566f043697ab486815fa6ee8
bettercallmefifi <ferdaouskai@gmail.com>
2025-07-27 15:00:36 +0100

    done

---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,15 @@
+#ifndef FT_H
+#define FT_H
+
+#include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+#include <stdio.h>
+
+void    ft_putchar(char c);
+void    ft_putstr(char *str);
+void    ft_swap(int *a, int *b);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
+
+#endif
